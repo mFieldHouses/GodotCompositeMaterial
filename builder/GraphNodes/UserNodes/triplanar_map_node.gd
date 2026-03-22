4a3225251a28f6dfe6988a59be4b6dbe262c30d8
mFieldHouses <mathias.veldhuizen@gmail.com>
2026-03-22 22:40:05 +0100

    round off properly implementing triplanar mapping

--- builder/GraphNodes/UserNodes/triplanar_map_node.gd
+++ builder/GraphNodes/UserNodes/triplanar_map_node.gd
@@ -7,9 +7,13 @@ var represented_configuration : CPMB_TriplanarUVConfiguration
 func _node_ready() -> void:
 	represented_configuration = CPMB_TriplanarUVConfiguration.new()
 	$mode.item_selected.connect(func(x): represented_configuration.space = x)
+	$blend.value_changed.connect(func(x): represented_configuration.blend = x)
 
 func get_represented_object(port_idx : int) -> Object:
 	return represented_configuration
 
 func set_represented_object(object : Object) -> void:
 	represented_configuration = object
+	
+	$mode.selected = object.space
+	$blend.value = object.blend
